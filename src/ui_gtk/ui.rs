3c9b3d97b4d7e4a397a5ee25b20184f7e7e5d8c4
Userwithaname <42674728+Userwithaname@users.noreply.github.com>
2025-10-07 19:40:38 +0200

    Visual UI improvements

--- src/ui_gtk/ui.rs
+++ src/ui_gtk/ui.rs
@@ -6,9 +6,6 @@ use std::sync::mpsc;
 
 use crate::player::{PlayerRequest, PlayerResponse};
 
-// TODO: Display currently playing song/album/atrist
-// TODO: Marquee long titles
-// TODO: Display album cover
 // TODO: When queue is empty, display a landing page
 pub fn build(
     app: &Application,
@@ -23,7 +20,24 @@ pub fn build(
         .valign(Align::Center)
         .halign(Align::Center)
         .orientation(Orientation::Vertical)
-        .spacing(12)
+        .spacing(6)
+        .build();
+
+    // TODO: Display the album cover
+
+    // TODO: Display currently playing song/album/atrist
+    // TODO: Marquee long titles
+    // let title_label = gtk::Label::builder().label("Song Title").build();
+    // let album_label = gtk::Label::builder().label("Album Title").build();
+    // let artist_label = gtk::Label::builder().label("Band Name").build();
+    // gtk_box_main.append(&title_label);
+    // gtk_box_main.append(&album_label);
+    // gtk_box_main.append(&artist_label);
+
+    // TODO: Overlay media controls & auto-hide
+    let gtk_box_media_toolbar = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .css_classes(["toolbar", "osd"])
         .build();
 
     let gtk_box_player_controls = gtk::Box::builder()
@@ -31,9 +45,11 @@ pub fn build(
         .spacing(12)
         .build();
 
-    // TODO: Symbolic icons
     // TODO: Disable buttons based on state (loading library, no queue, etc)
-    let prev_button = Button::builder().label("⏮").build();
+    let prev_button = Button::builder()
+        .css_classes(["circular"])
+        .icon_name("media-skip-backward-symbolic")
+        .build();
     prev_button.connect_clicked({
         let player_tx = player_tx.clone();
         move |_| {
@@ -43,23 +59,29 @@ pub fn build(
     gtk_box_player_controls.append(&prev_button);
 
     // TODO: Change symbol if stopped (like when the queue ends)
-    let pause_button = Button::builder().label("▶").build();
+    let pause_button = Button::builder()
+        .icon_name("media-playback-start-symbolic")
+        .css_classes(["circular"])
+        .build();
     pause_button.connect_clicked({
         let player_tx = player_tx.clone();
         move |button| {
             player_tx.send(PlayerRequest::PlayOrPause).unwrap();
             player_tx.send(PlayerRequest::GetCurrentState).unwrap();
             if let PlayerResponse::State(state) = ui_rx.recv().unwrap() {
-                button.set_label(match state {
-                    State::Playing => "⏸",
-                    _ => "▶",
+                button.set_icon_name(match state {
+                    State::Playing => "media-playback-pause-symbolic",
+                    _ => "media-playback-start-symbolic",
                 });
             }
         }
     });
     gtk_box_player_controls.append(&pause_button);
 
-    let next_button = Button::builder().label("⏭").build();
+    let next_button = Button::builder()
+        .css_classes(["circular"])
+        .icon_name("media-skip-forward-symbolic")
+        .build();
     next_button.connect_clicked({
         let player_tx = player_tx.clone();
         move |_| {
@@ -68,23 +90,27 @@ pub fn build(
     });
     gtk_box_player_controls.append(&next_button);
 
-    gtk_box_main.append(&gtk_box_player_controls);
-
-    // TODO: Seek bar/slider
-    // let seek_bar = Scale::builder()
-    //     .fill_level(0.0)
-    //     .orientation(Orientation::Horizontal)
-    //     .build();
+    // TODO: Responsive seek bar/slider
+    // TODO: Seek bar time labels
+    // let seek_bar = gtk::Scale::with_range(Orientation::Horizontal, 0.0, 1.0, 0.001);
     // seek_bar.connect_value_changed(move |scale| {
     //     println!("{}", scale.value());
     // });
-    // gtk_box_main.append(&seek_bar);
 
-    // let titlebar = gtk::Box::builder().build();
+    gtk_box_media_toolbar.append(&gtk_box_player_controls);
+    // gtk_box_media_toolbar.append(&seek_bar);
+
+    gtk_box_main.append(&gtk_box_media_toolbar);
+
+    let titlebar = adw::HeaderBar::builder()
+        .show_title(false)
+        .css_classes(["flat"])
+        .build();
+
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Mellow")
-        // .titlebar(&titlebar)
+        .titlebar(&titlebar)
         .child(&gtk_box_main)
         .build();
     window.present();
