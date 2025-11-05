4f2dec3036906cedb44d7e92a5c1ba8a57d7fc88
Userwithaname <42674728+Userwithaname@users.noreply.github.com>
2025-11-05 23:21:51 +0100

    Queue initialization cleanup

--- src/main.rs
+++ src/main.rs
@@ -42,53 +42,68 @@ fn init(app: &Application) {
 }
 
 fn init_player_queue(player: &mut Player, ui_tx: tokio_mpsc::Sender<UpdateUI>) {
+    if let Some(queue) = queue_from_args() {
+        player.queue.load_new(queue, None, None).unwrap();
+        return;
+    }
+
+    let mut library = Library::load_or_init(ui_tx).expect("Library could not be initialized");
+    let runtime = tokio::runtime::Runtime::new()
+        .map_err(|e| e.to_string())
+        .unwrap();
+    let library = runtime.block_on(async move {
+        library.rebuild().await.unwrap();
+        library
+    });
+
+    // TODO: Once the library works, don't load all tracks into the queue,
+    // but instead either restore the previous session or open the library
+    // without loading a queue
+    if player.queue.is_empty() {
+        let songs = library
+            .songs
+            .iter()
+            .map(|song| QueueItem::Song(Arc::new(Mutex::new(song.clone()))))
+            .collect();
+        player.queue.load_new(songs, Some(true), None).unwrap();
+    }
+}
+
+fn queue_from_args() -> Option<Vec<QueueItem>> {
     let mut args = std::env::args();
     args.next();
-    if args.len() > 0 {
-        let queue = Arc::new(Mutex::new(Some(Vec::new())));
-        args.for_each(|file| {
-            let path = Path::new(&file);
-            if path.is_file() {
-                // Add files from arguments to queue
-                if let Ok(song) = Song::new(&file, None) {
-                    if !Library::file_supported(&file) {
+
+    if args.len() == 0 {
+        return None;
+    }
+
+    let queue = Arc::new(Mutex::new(Some(Vec::new())));
+    args.for_each(|file| {
+        let path = Path::new(&file);
+        if path.is_file() {
+            // Add files from arguments to queue
+            if let Ok(song) = Song::new(&file, None) {
+                if !Library::file_supported(&file) {
+                    return;
+                }
+                let song = QueueItem::Song(Arc::new(Mutex::new(song)));
+                queue.lock().unwrap().as_mut().unwrap().push(song);
+            }
+        } else if Path::exists(path) {
+            // Add all files within directory arguments to queue
+            let _ = visit_dirs(path, &|file| {
+                let file = file.path();
+                let file = file.to_str().unwrap();
+                if let Ok(song) = Song::new(file, None) {
+                    if !Library::file_supported(file) {
                         return;
                     }
                     let song = QueueItem::Song(Arc::new(Mutex::new(song)));
                     queue.lock().unwrap().as_mut().unwrap().push(song);
                 }
-            } else if Path::exists(path) {
-                // Add all files within directory arguments to queue
-                let _ = visit_dirs(path, &|file| {
-                    let file = file.path();
-                    let file = file.to_str().unwrap();
-                    if let Ok(song) = Song::new(file, None) {
-                        if !Library::file_supported(file) {
-                            return;
-                        }
-                        let song = QueueItem::Song(Arc::new(Mutex::new(song)));
-                        queue.lock().unwrap().as_mut().unwrap().push(song);
-                    }
-                });
-            }
-        });
-        player
-            .queue
-            .load_new(queue.lock().unwrap().take().unwrap(), None, None)
-            .unwrap();
-    } else {
-        let mut library = Library::load_or_init(ui_tx).expect("Library could not be initialized");
-        let runtime = tokio::runtime::Runtime::new()
-            .map_err(|e| e.to_string())
-            .unwrap();
-        let songs = runtime.block_on(async move {
-            library.rebuild().await.unwrap();
-            library
-                .songs
-                .iter()
-                .map(|song| QueueItem::Song(Arc::new(Mutex::new(song.clone()))))
-                .collect()
-        });
-        player.queue.load_new(songs, Some(true), None).unwrap();
-    }
+            });
+        }
+    });
+
+    Some(queue.lock().unwrap().take().unwrap())
 }
